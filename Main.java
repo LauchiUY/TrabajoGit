f5f7cec2463081d0bdd9a25a4e9db879937c491b
Lauchi2 <lautarocuadrado2021@gmail.com>
2025-03-10 20:10:17 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main (String[] args){
+        CodigoParaRefactorizar codigo = new CodigoParaRefactorizar();
+
+        codigo.calculoIva(55.5);
+    }
+} 
